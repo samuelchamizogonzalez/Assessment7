ca9502232dd11ca7734a83a097c45730ce7261f0
Samuel.Chamizo <Samuel.Chamizo@solera.com>
2022-12-05 14:34:40 +0100

    Assessment7 - Creation backend proyect

---
+++ backend/Assessment7/src/test/java/Bootcamp/Assessment7/Assessment7ApplicationTests.java
@@ -0,0 +1,13 @@
+package Bootcamp.Assessment7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assessment7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
